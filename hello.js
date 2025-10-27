65e5aafc0d52832b0811db4d246e2e3d33a42976
Marcus <mvddssarmento@hotmail.com>
2025-10-27 20:31:51 -0300

    Feat: Aula 2 - NPM

--- hello.js
+++ hello.js
@@ -1,8 +1,12 @@
 var http = require('http');
+var dayjs = require('dayjs');
+var localizedFormat = require('dayjs/plugin/localizedFormat');
+
+dayjs.extend(localizedFormat);
 
 function servercaallback(req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello " + process.argv[2]);
+    res.write("Hello " + process.argv[2] + ", hoje eh " + dayjs().format('LLL'));
     res.end();
 }
 
